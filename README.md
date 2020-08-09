f2f0da9f63ae8585c0afb2ab8642c2a0e8ee3bea
youngkiu <youngkiu@gmail.com>
2020-08-09 20:04:18 +0900

    implement the library seating arrangement program for COVID-19

---
+++ README.md
@@ -0,0 +1,6 @@
+# covid19_library_seat
+
+### Library seating arrangement for COVID-19
+
+![screen_shot](./covid19_library_seat.png)
+ 
---
+++ wxFormBuilder/noname.py
@@ -0,0 +1,155 @@
+# -*- coding: utf-8 -*-
+
+###########################################################################
+## Python code generated with wxFormBuilder (version 3.9.0 Jul 23 2020)
+## http://www.wxformbuilder.org/
+##
+## PLEASE DO *NOT* EDIT THIS FILE!
+###########################################################################
+
+import wx
+import wx.xrc
+import wx.grid
+
+###########################################################################
+## Class MyFrame1
+###########################################################################
+
+class MyFrame1 ( wx.Frame ):
+
+	def __init__( self, parent ):
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 1032,535 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+
+		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
+
+		bSizer2 = wx.BoxSizer( wx.VERTICAL )
+
+		gbSizer1 = wx.GridBagSizer( 0, 0 )
+		gbSizer1.SetFlexibleDirection( wx.BOTH )
+		gbSizer1.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_SPECIFIED )
+
+		self.m_staticText4 = wx.StaticText( self, wx.ID_ANY, u"앞뒤 빈자리:", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText4.Wrap( -1 )
+
+		gbSizer1.Add( self.m_staticText4, wx.GBPosition( 0, 0 ), wx.GBSpan( 1, 1 ), wx.ALIGN_CENTER|wx.ALL, 5 )
+
+		self.m_checkBox1 = wx.CheckBox( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_checkBox1.SetValue(True)
+		gbSizer1.Add( self.m_checkBox1, wx.GBPosition( 0, 1 ), wx.GBSpan( 1, 1 ), wx.ALIGN_CENTER|wx.ALL, 5 )
+
+		self.m_staticText1 = wx.StaticText( self, wx.ID_ANY, u"옆으로 빈자리:", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText1.Wrap( -1 )
+
+		gbSizer1.Add( self.m_staticText1, wx.GBPosition( 0, 2 ), wx.GBSpan( 1, 1 ), wx.ALIGN_CENTER|wx.ALL, 5 )
+
+		self.m_textCtrl1 = wx.TextCtrl( self, wx.ID_ANY, u"2", wx.DefaultPosition, wx.Size( 20,-1 ), 0 )
+		gbSizer1.Add( self.m_textCtrl1, wx.GBPosition( 0, 3 ), wx.GBSpan( 1, 1 ), wx.ALIGN_CENTER|wx.ALL, 5 )
+
+		self.m_staticText2 = wx.StaticText( self, wx.ID_ANY, u"재사용 불가 시간:", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText2.Wrap( -1 )
+
+		gbSizer1.Add( self.m_staticText2, wx.GBPosition( 0, 4 ), wx.GBSpan( 1, 1 ), wx.ALIGN_CENTER|wx.ALL, 5 )
+
+		self.m_textCtrl2 = wx.TextCtrl( self, wx.ID_ANY, u"3", wx.DefaultPosition, wx.Size( 20,-1 ), 0 )
+		gbSizer1.Add( self.m_textCtrl2, wx.GBPosition( 0, 5 ), wx.GBSpan( 1, 1 ), wx.ALIGN_CENTER|wx.ALL, 5 )
+
+		self.m_staticText3 = wx.StaticText( self, wx.ID_ANY, u"최대 사용률(%):", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText3.Wrap( -1 )
+
+		gbSizer1.Add( self.m_staticText3, wx.GBPosition( 0, 6 ), wx.GBSpan( 1, 1 ), wx.ALIGN_CENTER|wx.ALL, 5 )
+
+		self.m_textCtrl3 = wx.TextCtrl( self, wx.ID_ANY, u"30", wx.DefaultPosition, wx.Size( 30,-1 ), 0 )
+		gbSizer1.Add( self.m_textCtrl3, wx.GBPosition( 0, 7 ), wx.GBSpan( 1, 1 ), wx.ALIGN_CENTER|wx.ALL, 5 )
+
+		self.m_button1 = wx.Button( self, wx.ID_ANY, u"시작", wx.DefaultPosition, wx.Size( 50,-1 ), 0 )
+		gbSizer1.Add( self.m_button1, wx.GBPosition( 0, 8 ), wx.GBSpan( 1, 1 ), wx.ALIGN_CENTER|wx.ALL, 5 )
+
+		self.m_button3 = wx.Button( self, wx.ID_ANY, u"사용가능 자리", wx.DefaultPosition, wx.DefaultSize, 0 )
+
+		self.m_button3.SetDefault()
+		gbSizer1.Add( self.m_button3, wx.GBPosition( 0, 9 ), wx.GBSpan( 1, 1 ), wx.ALL, 5 )
+
+
+		bSizer2.Add( gbSizer1, 1, wx.ALIGN_CENTER, 5 )
+
+		sbSizer1 = wx.StaticBoxSizer( wx.StaticBox( self, wx.ID_ANY, u"열람실" ), wx.VERTICAL )
+
+		self.m_grid1 = wx.grid.Grid( sbSizer1.GetStaticBox(), wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, 0 )
+
+		# Grid
+		self.m_grid1.CreateGrid( 20, 12 )
+		self.m_grid1.EnableEditing( False )
+		self.m_grid1.EnableGridLines( True )
+		self.m_grid1.EnableDragGridSize( False )
+		self.m_grid1.SetMargins( 0, 0 )
+
+		# Columns
+		self.m_grid1.EnableDragColMove( False )
+		self.m_grid1.EnableDragColSize( False )
+		self.m_grid1.SetColLabelSize( 30 )
+		self.m_grid1.SetColLabelAlignment( wx.ALIGN_CENTER, wx.ALIGN_CENTER )
+
+		# Rows
+		self.m_grid1.EnableDragRowSize( False )
+		self.m_grid1.SetRowLabelSize( 30 )
+		self.m_grid1.SetRowLabelAlignment( wx.ALIGN_CENTER, wx.ALIGN_CENTER )
+
+		# Label Appearance
+
+		# Cell Defaults
+		self.m_grid1.SetDefaultCellAlignment( wx.ALIGN_CENTER, wx.ALIGN_CENTER )
+		sbSizer1.Add( self.m_grid1, 0, wx.ALIGN_CENTER|wx.ALL, 5 )
+
+
+		bSizer2.Add( sbSizer1, 1, wx.EXPAND, 5 )
+
+
+		self.SetSizer( bSizer2 )
+		self.Layout()
+		self.m_statusBar1 = self.CreateStatusBar( 1, wx.STB_SIZEGRIP, wx.ID_ANY )
+
+		self.Centre( wx.BOTH )
+
+		# Connect Events
+		self.m_button1.Bind( wx.EVT_BUTTON, self.m_button1OnButtonClick )
+		self.m_button3.Bind( wx.EVT_BUTTON, self.m_button3OnButtonClick )
+		self.m_grid1.Bind( wx.grid.EVT_GRID_CELL_LEFT_CLICK, self.m_grid1OnGridCellLeftClick )
+		self.m_grid1.Bind( wx.grid.EVT_GRID_CELL_RIGHT_CLICK, self.m_grid1OnGridCellRightClick )
+
+	def __del__( self ):
+		pass
+
+
+	# Virtual event handlers, overide them in your derived class
+	def m_button1OnButtonClick( self, event ):
+		event.Skip()
+
+	def m_button3OnButtonClick( self, event ):
+		event.Skip()
+
+	def m_grid1OnGridCellLeftClick( self, event ):
+		event.Skip()
+
+	def m_grid1OnGridCellRightClick( self, event ):
+		event.Skip()
+
+
+###########################################################################
+## Class MyPanel1
+###########################################################################
+
+class MyPanel1 ( wx.Panel ):
+
+	def __init__( self, parent, id = wx.ID_ANY, pos = wx.DefaultPosition, size = wx.Size( 500,300 ), style = wx.TAB_TRAVERSAL, name = wx.EmptyString ):
+		wx.Panel.__init__ ( self, parent, id = id, pos = pos, size = size, style = style, name = name )
+
+		bSizer1 = wx.BoxSizer( wx.VERTICAL )
+
+
+		self.SetSizer( bSizer1 )
+		self.Layout()
+
+	def __del__( self ):
+		pass
+
+
